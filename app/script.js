a2625d35786c3437f2258d9f03cbaa6b37ea2a1b
Michael Alicea <mfalicea@buffalo.edu>
2016-03-10 18:39:13 -0500

    Lets gooooo!! Problems solved, partials working, routes working.  Ready to start working on authentication!

--- app/script.js
+++ app/script.js
@@ -3,22 +3,35 @@ var theApp = angular.module('theApp', ['ngRoute']);
 theApp.config(['$routeProvider',
   function($routeProvider) {
     $routeProvider.
+      //Can add in if user is autheticated here!
       when('/', {
-        templateUrl: 'index.html',
-        controller: 'mainController'
+        controller: 'mainCtrl'
       }).
-      when('/test', {
-        templateUrl: 'components/view1/view1.html',
-        controller: 'testController'
+    
+      when('/login', {
+        templateUrl: 'components/login/loginView.html',
+        controller: 'loginCtrl'
       }).
+
+      when('/dashboard', {
+        templateUrl: 'components/dashboard/dashboardView.html',
+        controller: 'dashboardCtrl'
+      }).
+
       otherwise({
         redirectTo: '/'
       });
+
   }]);
-theApp.controller('mainController', function($scope) {
-  $scope.message = 'Main Controller!';
+
+theApp.controller('mainCtrl', function($scope) {
+  $scope.message = "This is the main controller and can be on every page, if we want!"
 });
 
-theApp.controller('testController', function($scope) {
-  $scope.message = 'Testing 1-2-3';
+theApp.controller('loginCtrl', function($scope) {
+  $scope.message = 'Welcome to Login!';
+});
+
+theApp.controller('dashboardCtrl', function($scope) {
+  $scope.message = 'Welcome to the Dashboard!';
 });

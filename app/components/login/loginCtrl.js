58b3dabe8ade4bf99960e9ddadbcc13e5628efd3
Bobby Shannon <rshannon@buffalo.edu>
2016-03-14 22:21:26 -0400

    submitLogin callbacks now work after some tweaking https://github.com/lynndylanhurley/ng-token-auth/issues/118

--- app/components/login/loginCtrl.js
+++ app/components/login/loginCtrl.js
@@ -1,17 +1,16 @@
 theApp.controller('loginCtrl', function($scope,$auth) {
-  $scope.handleLoginBtnClick = function() {
-    $auth.submitLogin($scope.loginForm)
-    .then(function(resp) {
-      console.log("Logged in!?");
-      // handle success response
-    })
-    .catch(function(resp) {
-      console.log($scope.loginForm);
-      console.log("Obviously got an error response!");
-      $scope.loginErrorMessage = "Sorry, but uh we couldn't find you, try again";
-      // handle error response
-    });
-  };
+	$scope.login = function() {
+	    $auth.submitLogin($scope.loginForm)
+	    .then(function(resp) {
+	      // handle success response
+	      console.log("Logged in!?");
+	    })
+	    .catch(function(resp) {
+	      // handle error response
+	      console.log("Obviously got an error response!");
+	      $scope.loginErrorMessage = "Sorry, but uh we couldn't find you, try again";
+	    });
+	};
 });
 
 

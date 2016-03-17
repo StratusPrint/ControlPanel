311d339eb8908159a5244139b5e3a79d6250a3d6
shiraz <shirazam@buffalo.edu>
2016-03-17 16:12:33 +0000

    Added redirection

--- app/script.js
+++ app/script.js
@@ -15,7 +15,20 @@ theApp.config(['$routeProvider',
 
       when('/dashboard', {
         templateUrl: 'components/dashboard/dashboardView.html',
-        controller: 'dashboardCtrl'
+        controller: 'dashboardCtrl',
+        resolve: {
+          auth: ['$auth', function($auth) {
+
+            var aut = $auth.validateUser();
+
+            return aut;
+          }]
+        }
+      }).
+
+      when('/register',{
+        templateUrl: 'components/register/registerView.html',
+        controller: 'registerCtrl'
       }).
 
       otherwise({
@@ -24,15 +37,51 @@ theApp.config(['$routeProvider',
 
   }]);
 
-  theApp.config(function($authProvider) {
+theApp.config(function($authProvider) {
     $authProvider.configure({
-      //apiUrl: 'https://dev.api.stratusprint.com/v1'
-      apiUrl: 'http://localhost:8081/v1'
+      apiUrl: 'https://dev.api.stratusprint.com/v1',
+      tokenValidationPath:     '/auth/validate_token',
+      signOutUrl:              '/auth/sign_out',
+      emailRegistrationPath:   '/auth',
+      confirmationSuccessUrl:  '/dashboard',
+      emailSignInPath:         '/auth/sign_in',
+      storage:                 'cookies',
+      forceValidateToken:      false,
+      //apiUrl: 'http://localhost:8081/v1'
     });
   });
 
-  theApp.controller('mainCtrl', function($scope) {
+theApp.controller('mainCtrl', function($scope) {
     $scope.message =
       'This is the main controller and can be on every page, if we want!';
   });
 
+/*Setting up authentication, redirections, and signout*/
+theApp.run(function($rootScope, $location, $auth) {
+
+  $rootScope.$on('auth:invalid',function(e) {
+    $location.path('/login');
+  });
+
+  $rootScope.$on('auth:login-success',function(e) {
+    $location.path('/dashboard');
+  });
+
+  $rootScope.signout = function() {
+    $auth.signOut()
+        .then(function(resp) {
+          alert('You were successfully logged out');
+          $location.path('/');
+        })
+        .catch(function(resp) {
+          // handle error response
+          alert('You were successfully logged out');
+          console.log(resp);
+        });
+  };
+
+  $rootScope.$on('auth:logout-error', function(ev, reason) {
+    alert('logout failed because ' + reason.errors[0]);
+  });
+
+});

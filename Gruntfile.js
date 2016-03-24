a290cd61a29ab7efaa9031f88c61e59a7de85645
Bob Shannon <rshannon@buffalo.edu>
2016-03-24 20:30:47 +0000

    Further simplify directory structure; use ngAnnotate to preprocess angular assets before minification; various other tweaks

--- Gruntfile.js
+++ Gruntfile.js
@@ -21,12 +21,15 @@ module.exports = function(grunt) {
     },
     clean: {
       src: [
-        'app/assets/public/**/*.css',
-        'app/assets/public/**/*.js'
+        'app/public/**/*.css',
+        'app/public/**/*.js'
       ]
     },
     jshint: {
-      src: ["app/assets/components/**/*.js","app/script.js"],
+      src: [
+        "app/components/**/*.js",
+        "app/app.js"
+      ],
       options: {
         reporter: require('jshint-stylish'),
         globals: {
@@ -37,12 +40,25 @@ module.exports = function(grunt) {
 
     },
     jscs: {
-      src: ["app/assets/components/**/*.js","app/script.js"],
+      src: [
+        "app/assets/components/**/*.js",
+        "app/script.js"
+      ],
       options: {
         "config": ".jscsrc",
         "preset": "google",
         "fix": true 
       }
+    },
+    ngAnnotate: {
+        angular_app: {
+            files: {
+              'app/public/js/app.js': [
+                'app/app.js',
+                'app/components/**/*.js'
+              ]
+            }
+        },
     },
     uglify: {
       options: {
@@ -53,44 +69,23 @@ module.exports = function(grunt) {
       },
       base: {
         files: {
-          'app/assets/public/js/base.js': [
-            'app/assets/bower_components/jquery/dist/jquery.js',
-            'app/assets/bower_components/bootstrap/dist/js/bootstrap.min.js',
-            'assets/bower_components/metisMenu/dist/metisMenu.min.js',
-            'assets/bower_components/raphael/raphael-min.js',
-            'assets/bower_components/morrisjs/morris.min.js',
-            'assets/dist/js/sb-admin-2.js',
-            'app/assets/bower_components/angular/angular.min.js',
-            'app/assets/bower_components/angular-route/angular-route.min.js',
-            'app/assets/bower_components/angular-cookie/angular-cookie.min.js',
-            'app/assets/bower_components/ng-token-auth/dist/ng-token-auth.min.js',
-            'assets/bower_components/flot/excanvas.min.js',
-            'assets/bower_components/flot/jquery.flot.js',
-            'assets/bower_components/flot/jquery.flot.pie.js',
-            'assets/bower_components/flot/jquery.flot.resize.js',
-            'assets/bower_components/flot/jquery.flot.time.js',
-            'assets/bower_components/flot.tooltip/js/jquery.flot.tooltip.min.js'
+          'app/public/js/base.js': [
+            'bower_components/jquery/dist/jquery.js',
+            'bower_components/bootstrap/dist/js/bootstrap.js',
+            'bower_components/startbootstrap-sb-admin-2/dist/js/sb-admin-2.js',
+            'bower_components/metisMenu/dist/metisMenu.js',
+            'bower_components/morrisjs/morris.js',
+            'bower_components/angular/angular.js',
+            'bower_components/angular-route/angular-route.js',
+            'bower_components/angular-cookie/angular-cookie.js',
+            'bower_components/ng-token-auth/dist/ng-token-auth.js'
           ]
         }
       },
-      misc: {
+      app: {
         files: {
-          'app/assets/public/js/miscScripts.js': [
-            'app/assets/js/*.js'
-          ]
-        }
-      },
-      dashboard: {
-        files: {
-          'app/assets/public/dashboard/dashboard.js': [
-            'app/components/dashboard/*.js'
-          ]
-        }
-      },
-      login: {
-        files: {
-          'app/assets/public/login/login.js': [
-            'app/components/login/*.js'
+          'app/public/js/app.js': [
+            'app/public/js/app.js'
           ]
         }
       }
@@ -102,18 +97,12 @@ module.exports = function(grunt) {
       },
       base: {
         files: {
-          'app/assets/public/css/base.css': [
-            'app/assets/bower_components/bootstrap/dist/css/bootstrap.min.css',
-            'app/assets/bower_components/metisMenu/dist/metisMenu.min.css',
-            'app/assets/bower_components/font-awesome/css/font-awesome.min.css',
-            'app/assets/dist/css/sb-admin-2.css'
-          ]
-        }
-      },
-      main: {
-        files: {
-          'app/assets/public/css/styles.css': [
-            'app/assets/css/*.css'
+          'app/public/css/base.css': [
+            'bower_components/bootstrap/dist/css/bootstrap.css',
+            'bower_components/metisMenu/dist/metisMenu.css',
+            'bower_components/startbootstrap-sb-admin-2/dist/css/timeline.css',
+            'bower_components/startbootstrap-sb-admin-2/dist/css/sb-admin-2.css',
+            'bower_components/font-awesome/css/font-awesome.css',
           ]
         }
       }
@@ -121,19 +110,18 @@ module.exports = function(grunt) {
     watch: {
       scripts: {
         files: [
-          'app/*.js',
-          'app/assets/js/*.js',
-          'app/components/**/*.js'
+          'app/app.js',
+          'app/components/**/*.js',
+          'app/shared/**/*.js'
         ],
-        tasks: ['jshint','jscs'],
+        tasks: ['default'],
         options: {
           spawn: false,
         },
       },
       styles: {
         files: [
-          'app/assets/css/*.css',
-          'app/components/**/*.css'
+          'app/assets/css/**/*.css'
         ],
         tasks: ['cssmin'],
         options: {
@@ -144,6 +132,7 @@ module.exports = function(grunt) {
   });
 
   grunt.registerTask('default', [
+    'ngAnnotate:angular_app',
     'newer:uglify',
     'newer:cssmin',
     'newer:jshint',

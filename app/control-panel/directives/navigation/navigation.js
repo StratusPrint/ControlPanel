d3f0da3e12ce8398837d2de687f0cbdb2e7e804b
Bob Shannon <rshannon@buffalo.edu>
2016-04-02 22:29:34 +0000

    Hide navigation if not logged in

--- app/control-panel/directives/navigation/navigation.js
+++ app/control-panel/directives/navigation/navigation.js
@@ -1,7 +1,6 @@
 app.directive('navigation', function() {
   return {
     restrict: 'E',
-    scope: {},
     templateUrl: 'control-panel/directives/navigation/navigation.html',
     link: function(scope, elem, attrs) {
       angular.element('#side-menu').metisMenu();

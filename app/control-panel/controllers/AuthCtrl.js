c65e899ad0f6950545350b8d38a82a3eb2d4b17a
Bob Shannon <rshannon@buffalo.edu>
2016-04-02 23:31:39 +0000

    Show login error messages

--- app/control-panel/controllers/AuthCtrl.js
+++ app/control-panel/controllers/AuthCtrl.js
@@ -3,6 +3,10 @@ app.controller('AuthCtrl', AuthCtrl);
 AuthCtrl.$inject = ['$scope', 'auth'];
 
 function AuthCtrl($scope, auth) {
+  $scope.$on('auth:login-error', function(ev, reason) {
+    $scope.error = reason.errors.toString();
+  });
+
   $scope.login = function(user) {
     auth.login(user);
   };

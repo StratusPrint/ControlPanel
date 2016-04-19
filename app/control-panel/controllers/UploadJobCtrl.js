be1596ab9f9ca63a292651a6cbd8932cd54a0741
Bob Shannon <rshannon@buffalo.edu>
2016-04-19 05:28:04 +0000

    Allow *.stl and *.gcode files to be uploaded

--- app/control-panel/controllers/UploadJobCtrl.js
+++ app/control-panel/controllers/UploadJobCtrl.js
@@ -2,29 +2,51 @@
 
 app.controller('UploadJobCtrl', UploadJobCtrl);
 
-UploadJobCtrl.$inject = ['$scope', '$state', '$stateParams', '$timeout', 'Upload'];
+UploadJobCtrl.$inject = ['$scope', '$state', '$stateParams', '$timeout', '$http', 'Upload'];
 
-function UploadJobCtrl($scope, $state, $stateParams, $timeout, Upload) {
+function UploadJobCtrl($scope, $state, $stateParams, $timeout, $http, Upload) {
   $scope.uploadFiles = function(files, errFiles) {
     $scope.files = files;
     $scope.errFiles = errFiles;
     angular.forEach(files, function(file) {
-      file.upload = Upload.upload({
-        url: 'https://angular-file-upload-cors-srv.appspot.com/upload',
-        data: {file: file},
-      });
+        Upload.base64DataUrl(file).then(function(url) {
 
-      file.upload.then(function(response) {
-        $timeout(function() {
-          file.result = response.data;
+          var extension = file.name.split('.').pop();
+
+          if (extension === 'stl') {
+            url = url.substr(0, 5) + 'application/stl' + url.substr(5);
+          } else if (extension === 'gcode') {
+            url = url.substr(0, 5) + 'application/gcode' + url.substr(5);
+          }
+
+          /*file.upload = $http({
+            method: 'POST',
+            url: 'https://dev.api.stratusprint.com/v1/printers/12/jobs',
+            data: {model: url},
+          });*/
+
+          file.upload = Upload.http({
+            url: 'https://dev.api.stratusprint.com/v1/printers/12/jobs',
+            headers: {'Content-Type': 'application/json' },
+            data: {
+              model: url,
+            },
+          });
+
+          file.upload.then(function(response) {
+            $timeout(function() {
+              console.log('timeout');
+              file.result = response.data;
+            });
+          }, function(response) {
+            if (response.status > 0) {
+              $scope.errorMsg = response.status + ': ' + response.data;
+            }
+          }, function(evt) {
+            file.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
+          });
+
         });
-      }, function(response) {
-        if (response.status > 0) {
-          $scope.errorMsg = response.status + ': ' + response.data;
-        }
-      }, function(evt) {
-        file.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
       });
-    });
   };
 }

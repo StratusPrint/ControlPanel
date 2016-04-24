ddc116f5e908be5f8c90828db58c360109d0653e
Michael Alicea <mfalicea@buffalo.edu>
2016-04-24 18:54:27 +0000

    Set default hub in the profile page
    Doesn't work, I have no idea why.  it is correctly setting the
    profile.default_hub_id to the selected one, it is correctly
    sending that into the auth.updateAccount, it is correctly changing
    the users name.  But when it comes to setting the default hub it
    refuses.  I have tried it passing in as a string, and as of now
    it is casting the value.

--- app/control-panel/controllers/DashboardCtrl.js
+++ app/control-panel/controllers/DashboardCtrl.js
@@ -1,7 +1,15 @@
 app.controller('DashboardCtrl', DashboardCtrl);
 
-DashboardCtrl.$inject = ['$scope'];
+DashboardCtrl.$inject = ['$scope', 'hub', 'printer', 'sensor'];
 
-function DashboardCtrl($scope) {
+function DashboardCtrl($scope, hub, printer, sensor) {
   $scope.message = 'Welcome to the Dashboard!';
+  console.log('Default Hub: ' + $scope.user._user.default_hub_id);
+  // var defaultHubPromise = hub.getHub($scope.user._user.default_hub_id);
+  var defaultHubPromise = hub.getHub(27);
+
+  DefaultHubPromise.then(function(response) {
+    $scope.hub = response;
+    console.log(JSON.stringify($scope.hub));
+  });
 }

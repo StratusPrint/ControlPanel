c019e79718ed11fc110edd265484392ee7895497
Michael Alicea <mfalicea@buffalo.edu>
2016-04-24 18:59:30 +0000

    Fix error on dashboard

--- app/control-panel/controllers/DashboardCtrl.js
+++ app/control-panel/controllers/DashboardCtrl.js
@@ -5,10 +5,11 @@ DashboardCtrl.$inject = ['$scope', 'hub', 'printer', 'sensor'];
 function DashboardCtrl($scope, hub, printer, sensor) {
   $scope.message = 'Welcome to the Dashboard!';
   console.log('Default Hub: ' + $scope.user._user.default_hub_id);
-  // var defaultHubPromise = hub.getHub($scope.user._user.default_hub_id);
+  console.log('Fetching Hub id 27');
+  // Var defaultHubPromise = hub.getHub($scope.user._user.default_hub_id);
   var defaultHubPromise = hub.getHub(27);
 
-  DefaultHubPromise.then(function(response) {
+  defaultHubPromise.then(function(response) {
     $scope.hub = response;
     console.log(JSON.stringify($scope.hub));
   });

50db7addbe802755959643d5201361ef16bcb615
Michael Alicea <mfalicea@buffalo.edu>
2016-04-25 11:27:34 +0000

    Prepopulate select form with default hub

--- app/control-panel/controllers/ProfileCtrl.js
+++ app/control-panel/controllers/ProfileCtrl.js
@@ -7,12 +7,27 @@ function ProfileCtrl($scope, $controller, auth, hub) {
   $controller('AlertCtrl', { $scope: $scope });
 
   var hubsPromise = hub.getAllHubs();
-
   hubsPromise.then(function(response) {
     $scope.hubs = response;
   });
 
-  console.log('User: ' + JSON.stringify($scope.user));
+  /**
+   * SetCurrentHub
+   * Sets $scope.currentHub
+   * Called on initialization of the page to set the default value of the select field
+   * Called on save of the page to set default value of the select field
+   *
+   * @param hubId
+   * @returns {undefined}
+   */
+  $scope.setCurrentHub = function(hubId) {
+    var hubPromise = hub.getHub(hubId);
+    hubPromise.then(function(response) {
+      $scope.currentHub = response;
+    });
+  };
+
+  $scope.setCurrentHub($scope.user._user.default_hub_id);
 
   $scope.save = function(profile) {
     // Check whether any validation errors are present on the form
@@ -36,17 +51,17 @@ function ProfileCtrl($scope, $controller, auth, hub) {
 
     // Update user profile
     auth.updateAccount(profile)
-      .then(function(resp) {
-        profile.default_hub_id = Number(profile.default_hub_id);
-        console.log('Submitted profile: ' + JSON.stringify(profile));
-        $scope.addAlert('success', 'Profile successfully updated.');
-        $scope.resetForm();
-      })
-      .catch(function(resp) {
-        angular.forEach(resp.data.errors.full_messages, function(key, value) {
-          $scope.addAlert('danger', key);
-        });
+    .then(function(resp) {
+      profile.default_hub_id = Number(profile.default_hub_id);
+      console.log('Submitted profile: ' + JSON.stringify(profile));
+      $scope.addAlert('success', 'Profile successfully updated.');
+      $scope.resetForm();
+    })
+    .catch(function(resp) {
+      angular.forEach(resp.data.errors.full_messages, function(key, value) {
+        $scope.addAlert('danger', key);
       });
+    });
   };
 
   /**

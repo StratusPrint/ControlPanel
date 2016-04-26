dafd3c305d9d932e25e0544218a95e119c343ccd
shirazamin <shirazam@buffalo.edu>
2016-04-26 01:47:10 -0400

    Formatted the table and it now uses datatables

--- app/control-panel/controllers/UsersCtrl.js
+++ app/control-panel/controllers/UsersCtrl.js
@@ -1,61 +1,173 @@
 app.controller('UsersCtrl', UsersCtrl);
 
-UsersCtrl.$inject = ['$scope', '$state','$stateParams', 'admin'];
+UsersCtrl.$inject = ['$scope', '$state','$stateParams', 'admin','$controller','$compile', 'DTOptionsBuilder', 'DTColumnBuilder'];
 
-function UsersCtrl($scope, $state, $stateParams, admin) {
+function UsersCtrl($scope, $state, $stateParams, admin, $controller, $compile, DTOptionsBuilder, DTColumnBuilder) {
+
+  // Inject alert controller scope
+  $controller('AlertCtrl', { $scope: $scope });
+  var dtCtrl = this;
+
+  /******************************************************
+   *
+   * DataTables funsies
+   */
 
   /*
-  * Load the Users from the Database
-  */
-  function loadUsers() {
+   * Starts building the DataTable when the function returns!
+   * Fetches promise and waits until it is fulfilled
+   */
+  dtCtrl.dtOptions = DTOptionsBuilder.fromFnPromise(function() {
+    return admin.getAllUsers();
+  })
+  .withPaginationType('simple_numbers')
+  .withOption('createdRow',createdRow)
+  .withOption('responsive', true);
+
+
+  /*
+   * Scoops up the data returned from the promise
+   * Sets columns and fills data based off of that
+   */
+  dtCtrl.cols = [
+    DTColumnBuilder.newColumn('id').withTitle('ID').withOption('responsivePriority',3),
+    DTColumnBuilder.newColumn('name').withTitle('Name').withOption('responsivePriority',5),
+    DTColumnBuilder.newColumn('email').withTitle('Email').withOption('responsivePriority',2),
+    DTColumnBuilder.newColumn(null).withTitle('Admin').notSortable().renderWith(adminHTML).withOption('responsivePriority',7),
+    DTColumnBuilder.newColumn(null).withTitle('Last Sign In').renderWith(lastSignIn).withOption('responsivePriority',6),
+    DTColumnBuilder.newColumn(null).withTitle('Created at').renderWith(createdAt).withOption('responsivePriority',4),
+    DTColumnBuilder.newColumn(null).withTitle('Actions').notSortable().renderWith(actionButtonHTML).withOption('responsivePriority',1),
+    // DTColumnBuilder.newColumn(null).withTitle('').notSortable().renderWith(deleteButtonHTML).withOption('responsivePriority',1),
+  ];
+  dtCtrl.reloadData = reloadData;
+  dtCtrl.dtInstance = {};
+
+
+  function adminHTML(data) {
+
+    if (data.admin) {
+      return '<i class="fa fa-check"></i>';
+    }
+    return '<i  class="fa fa fa-close"></i>';
+  }
+
+  function deleteButtonHTML(data) {
+    return '<button class="btn btn-danger" ng-click="deleteModal(' + data.id + ')"><i class="fa fa-trash-o fa-fw"></i>Delete User</button>';
+  }
+
+  function actionButtonHTML(data, type, full, meta) {
+    return '<button class="btn btn-warning" ng-click=""><i class="fa fa-edit fa-fw"></i></button>&nbsp;' +
+    '<button class="btn btn-danger" ng-click="deleteModal(' + meta.row + ')"><i class="fa fa-trash-o fa-fw"></i></button>';
+  }
+
+  function lastSignIn(data) {
+    if (data.last_sign_in_at === null) {
+      return 'Has not signed in';
+    }
+    var d = new Date(data.last_sign_in_at);
+    return d.toLocaleDateString() + ' ' + d.toLocaleTimeString();
+  }
+
+  function createdAt(data){
+    var d = new Date(data.created_at);
+    return d.toLocaleDateString() + ' ' + d.toLocaleTimeString();
+  }
+
+  /**
+   * ReloadData
+   *
+   * @returns {undefined}
+   */
+  function reloadData() {
+    var resetPaging = true;
     var userPromise = admin.getAllUsers();
     userPromise.then(function(response) {
-      $scope.users = []; // To empty before reloading
-      var i;
-      for (i = 0 ; i < response.length; i++) {
-        $scope.users.push(response[i]);
-      }
-    }).catch(function(response) {
-      addErrorAlert(response.data);
+      dtCtrl.dtInstance.reloadData(response, resetPaging);
     });
+  }
+
+  /**
+   * CreatedRow
+   * Lets datatables know what row just got created
+   * Recompiles the table so that a directive can get binded to the DT
+   * It is used to bind the data to the directive
+   * Without this ng-click will not work
+   *
+   * @param row
+   * @param data
+   * @param dataIndex
+   * @returns {undefined}
+   */
+  function createdRow(row, data, dataIndex) {
+    $compile(angular.element(row).contents())($scope);
   }
 
   $scope.register = function(user) {
 
-    var registerPromise = admin.registerUser(user);
-    registerPromise.then(function(response) {
-      addSuccessAlert('User Successfully Registered!');
-      loadUsers();
-    })
-    .catch(function(response) {
-      var errors = response.data.errors.full_messages;
-      var i;
-      for (i = 0 ; i < errors.length ;i++) {
-        addErrorAlert(errors[i]);
-      }
-    });
+    if (!$scope.registerUser.$valid) {
+      $scope.addAlert('danger', 'Please correct the errors below and try submitting the form again.');
+      return;
+    }
+
+    // Check whether form has not been filled out
+    if ($scope.registerUser.$pristine) {
+      $scope.addAlert('warning', 'Please fill out the form below before saving.');
+      return;
+    }
+
+    if ($scope.user.isAdmin()) {
+      var registerPromise = admin.registerUser(user);
+      registerPromise.then(function(response) {
+        if (typeof (response) === 'object') {
+          $scope.addAlert('success', 'The user was successfully added');
+          $scope.resetForm();
+          reloadData();
+        } else {
+          $scope.addAlert('danger', 'Sorry but this user could not be added.  Some values are unprocessable');
+        }
+      })
+      .catch(function(response) {
+        var error = response.data.errors.full_messages[0];
+        $scope.addAlert('danger',error);
+      });
+      return;
+    }
+    $scope.addAlert('warning', 'Sorry, but you are not an admin.');
+  };
+
+
+  /**
+   * ResetForm
+   * clears the $scope.hub and sets the form to pristine(cleared)
+   *
+   * @returns {undefined}
+   */
+  $scope.resetForm = function() {
+    $scope._user = {};
+    $scope.registerUser.$setPristine();
   };
 
   $scope.deleteUser = function() {
-    if ($scope.modalUserID >= 0) {
-      var deletePromise = admin.deleteUser(_id);
-      deletePromise.then(function() {
-        loadUsers();
-        addSuccessAlert('User Deleted!');
-      })
-      .catch(function() {
+    if ($scope.modalUser.id !== undefined) {
 
+      var deletePromise = admin.deleteUser($scope.modalUser.id);
+      deletePromise.then(function() {
+        reloadData();
+        $scope.addAlert('success', 'The user was successfully added');
+      })
+      .catch(function(response) {
+        $scope.addAlert('danger','User could not be deleted');
       });
     }
-    $scope.modalUserID = -1;
-
-
-
+    $scope.showDeleteModal = false;
+    $scope.modalUser = {};
   };
 
-  $scope.deleteModal = function(_id) {
+  $scope.deleteModal = function(_rowId) {
+    var user = dtCtrl.dtInstance.DataTable.rows(_rowId).data()[0];
+    if (user.id === undefined) return;
     $scope.showDeleteModal = !$scope.showDeleteModal;
-    $scope.modalUserID = _id;
+    $scope.modalUser = user;
   };
 
 
@@ -63,8 +175,5 @@ function UsersCtrl($scope, $state, $stateParams, admin) {
 
   $scope.showDeleteModal = false; // Set to true to show the delete confirmation modal
   $scope.showEditModal = false; // Set true to show Edit Modal
-  $scope.modalUserID = -1;
-  $scope.users = [];
-  loadUsers();
-
+  $scope.modalUser = {};
 }

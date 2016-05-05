d78449ff33ff7d65be6f83e53d650ff7635b841a
Michael Alicea <mfalicea@buffalo.edu>
2016-05-05 00:04:12 +0000

    Add delay on deletePrinter before calling $state.go to return to the hub page
    fixes #26

--- app/control-panel/controllers/PrinterCtrl.js
+++ app/control-panel/controllers/PrinterCtrl.js
@@ -2,9 +2,9 @@
 
 app.controller('PrinterCtrl', PrinterCtrl);
 
-PrinterCtrl.$inject = ['$scope', '$state', '$stateParams', '$controller', '$interval', 'printer'];
+PrinterCtrl.$inject = ['$scope', '$state', '$stateParams', '$timeout', '$controller', '$interval', 'printer'];
 
-function PrinterCtrl($scope, $state, $stateParams, $controller, $interval, printer) {
+function PrinterCtrl($scope, $state, $stateParams, $timeout, $controller, $interval, printer) {
 	$scope.recentJobs = [];
 	$scope.commands = [];
 	$scope.printer = [];
@@ -46,142 +46,144 @@ function PrinterCtrl($scope, $state, $stateParams, $controller, $interval, print
 	};
 
 	/**
-	 * Delete printer
-	 */
-	$scope.deletePrinter = function(printerId) {
-		printer.deletePrinter(printerId)
-			.success(function(response) {
-				$state.go('dashboard.viewHub', { hubId: $stateParams.hubId });
-			});
-	};
+   * Delete printer
+   */
+  $scope.deletePrinter = function(printerId) {
+    printer.deletePrinter(printerId)
+    .success(function(response) {
+      $timeout(function() {
+        $state.go('dashboard.viewHub', { hubId: $stateParams.hubId });
+      },500);
+    });
+  };
 
-	/**
-	 * Modal visibility toggling
-	 */
-	$scope.showPrinterModal = function() {
-		$scope.printerModalVisible = true;
-	};
+  /**
+   * Modal visibility toggling
+   */
+  $scope.showPrinterModal = function() {
+    $scope.printerModalVisible = true;
+  };
 
-	$scope.hidePrinterModal = function() {
-		$scope.printerModalVisible = false;
-		$scope.printerModal.alerts = [];
-	};
+  $scope.hidePrinterModal = function() {
+    $scope.printerModalVisible = false;
+    $scope.printerModal.alerts = [];
+  };
 
-	$scope.showCancelJobModal = function() {
-		$scope.cancelJobModalVisible = true;
-	};
+  $scope.showCancelJobModal = function() {
+    $scope.cancelJobModalVisible = true;
+  };
 
-	$scope.hideCancelJobModal = function() {
-		$scope.cancelJobModalVisible = false;
-	};
+  $scope.hideCancelJobModal = function() {
+    $scope.cancelJobModalVisible = false;
+  };
 
-	$scope.showDeletePrinterModal = function() {
-		$scope.deletePrinterModalVisible = true;
-	};
+  $scope.showDeletePrinterModal = function() {
+    $scope.deletePrinterModalVisible = true;
+  };
 
-	$scope.hideDeletePrinterModal = function() {
-		$scope.deletePrinterModalVisible = false;
-	};
+  $scope.hideDeletePrinterModal = function() {
+    $scope.deletePrinterModalVisible = false;
+  };
 
-	/**
-	 * Retrieve the current job associated with this printer
-	 */
-	$scope.getCurrentJob = function() {
-		printer.getCurrentJob($stateParams.printerId)
-			.success(function(response) {
-				$scope.currentJob = response;
-			})
-			.error(function(response) {
-				console.log('Unable to retrieve current job.');
-				console.log(response);
-			});
-	};
+  /**
+   * Retrieve the current job associated with this printer
+   */
+  $scope.getCurrentJob = function() {
+    printer.getCurrentJob($stateParams.printerId)
+    .success(function(response) {
+      $scope.currentJob = response;
+    })
+    .error(function(response) {
+      console.log('Unable to retrieve current job.');
+      console.log(response);
+    });
+  };
 
-	/**
-	 * Retrieve the commands issued to this printer
-	 */
-	$scope.getCommands = function() {
-		printer.getCommands($stateParams.printerId)
-			.success(function(response) {
-				$scope.commands = response;
-			})
-			.error(function(response) {
-				console.log('Unable to retrieve list of commands.');
-				console.log(response);
-			});
-	};
+  /**
+   * Retrieve the commands issued to this printer
+   */
+  $scope.getCommands = function() {
+    printer.getCommands($stateParams.printerId)
+    .success(function(response) {
+      $scope.commands = response;
+    })
+    .error(function(response) {
+      console.log('Unable to retrieve list of commands.');
+      console.log(response);
+    });
+  };
 
-	/**
-	 * Retrieve list of recent jobs associated with this printer
-	 */
-	$scope.getRecentJobs = function() {
-		printer.getRecentJobs($stateParams.printerId)
-			.success(function(response) {
-				$scope.recentJobs = response;
-			})
-			.error(function(response) {
-				console.log('Unable to retrieve list of recent jobs.');
-				console.log(response);
-			});
-	};
+  /**
+   * Retrieve list of recent jobs associated with this printer
+   */
+  $scope.getRecentJobs = function() {
+    printer.getRecentJobs($stateParams.printerId)
+    .success(function(response) {
+      $scope.recentJobs = response;
+    })
+    .error(function(response) {
+      console.log('Unable to retrieve list of recent jobs.');
+      console.log(response);
+    });
+  };
 
-	/**
-	 * Retrieve list of all completed jobs associated with this printer
-	 */
-	$scope.getPrinter = function() {
-		printer.getPrinter($stateParams.printerId)
-			.success(function(response) {
-				$scope.printer = response;
-			})
-			.error(function(response) {
-				console.log('Unable to retrieve printer.');
-				console.log(response);
-			});
-	};
+  /**
+   * Retrieve list of all completed jobs associated with this printer
+   */
+  $scope.getPrinter = function() {
+    printer.getPrinter($stateParams.printerId)
+    .success(function(response) {
+      $scope.printer = response;
+    })
+    .error(function(response) {
+      console.log('Unable to retrieve printer.');
+      console.log(response);
+    });
+  };
 
-	/**
-	 * Issue a printer command.
-	 */
-	$scope.issueCommand = function() {
-		if($scope.command.name === '') { return; }
+  /**
+   * Issue a printer command.
+   */
+  $scope.issueCommand = function() {
+    if($scope.command.name === '') { return; }
 
-		printer.issueCommand($stateParams.printerId, $scope.command.name)
-			.success(function(response) {
-				$scope.addAlert('info', 'The ' + $scope.command.name + ' command has been sent to the printer. Please wait a minute for the command be executed and the current job status updated.');
-			})
-			.error(function(response) {
-				$scope.addAlert('danger', 'Unable to issue command. Please try again.');
-				console.log(response);
-			});
-	};
+    printer.issueCommand($stateParams.printerId, $scope.command.name)
+    .success(function(response) {
+      $scope.addAlert('info', 'The ' + $scope.command.name + ' command has been sent to the printer. Please wait a minute for the command be executed and the current job status updated.');
+    })
+    .error(function(response) {
+      $scope.addAlert('danger', 'Unable to issue command. Please try again.');
+      console.log(response);
+    });
+  };
 
-	/**
-	 * Refresh data.
-	 */
-	$scope.refresh = function() {
-		$scope.getCurrentJob();
-		$scope.getCommands();
-		$scope.getPrinter();
-		$scope.getRecentJobs();	
-	};
+  /**
+   * Refresh data.
+   */
+  $scope.refresh = function() {
+    $scope.getCurrentJob();
+    $scope.getCommands();
+    $scope.getPrinter();
+    $scope.getRecentJobs();	
+  };
 
-	$controller('AlertCtrl', { $scope: $scope });
-	$controller('AlertCtrl', { $scope: $scope.printerModal});
+  $controller('AlertCtrl', { $scope: $scope });
+  $controller('AlertCtrl', { $scope: $scope.printerModal});
 
-	$scope.refresh();
-	
-	var timerPromise;
-	$scope.timer = function() {
-		timerPromise = $interval($scope.refresh, 2000);
-	};
+  $scope.refresh();
 
-	$scope.timer();
+  var timerPromise;
+  $scope.timer = function() {
+    timerPromise = $interval($scope.refresh, 2000);
+  };
 
-	$scope.$on('$destroy',function(){
-	    if(timerPromise) {
-	        $interval.cancel(timerPromise);   
-	    }
-	});
+  $scope.timer();
 
-	$scope.$watch('command.name', $scope.issueCommand);
+  $scope.$on('$destroy',function(){
+    if(timerPromise) {
+      $interval.cancel(timerPromise);   
+    }
+  });
+
+  $scope.$watch('command.name', $scope.issueCommand);
 }
